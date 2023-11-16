499a1313b9922bcb1f59afa36f22be5312a3c411
angelicanatl <135089209+angelicanatl@users.noreply.github.com>
2023-11-16 15:21:22 +0700

    tambah method multiply()

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,7 @@ public class Calculator{
         return a + b;
     }
 
-    public int minus(int a, int b){
-        return a + b;
+    public int multiply(int a, int b){
+        return a * b;
     }
 }
